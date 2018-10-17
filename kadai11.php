8ee8ff78e8e37cf829d1f704fb4f7ca62bdc776e
久貝智美 <tottomo7@gmail.com>
2018-10-17 19:46:56 +0900

    Update kadai11.php

---
+++ kadai11.php
@@ -0,0 +1,31 @@
+<?php
+class Pet {
+ protected $name;
+
+ public function __construct($name) {
+     $this->name = $name;
+ }
+ public function petName(){
+     echo "この子の名前は、$this->name です。";
+ }
+}
+
+class Dogaction extends Pet {
+    public function petName(){
+        echo "$this->name はワンと鳴いた";
+    }
+}
+class Cataction extends Pet {
+    public function petName(){
+        echo "$this->name はニャーと鳴いた";
+    }
+}
+
+
+
+$pochi = new Dogaction("ポチ");
+$tama = new Cataction("タマ");
+echo $pochi->petName();
+echo $tama->petName();
+echo $pochi->name;
+?>
